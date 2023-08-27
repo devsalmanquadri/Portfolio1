16687d1e28ff226d4577b55a94cbf80454a04a61
Coding Geek <secretofsk@gmail.com>
2023-08-27 22:15:21 +0530

    Portf

---
+++ script.js
@@ -0,0 +1,22 @@
+const menu2 = document.querySelector('.menu1')
+const navitems = document.querySelector('.navitems')
+
+const toggleNavbar = () => {
+  navitems.classList.toggle('active')
+}
+
+menu2.addEventListener('click', () => toggleNavbar())
+
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycby8rKRJR0ALHqzPeSBp5TVdAV6SnDMsRAorxB8P9Fe3Jfryn6R5iVdO7802FKvpqN_U/exec'
+const form = document.forms['form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert('Thank you! your form is submitted successfully.'))
+    .then(() => {
+      window.location.reload()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
